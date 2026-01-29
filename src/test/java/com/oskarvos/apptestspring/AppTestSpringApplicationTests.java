2dab6c8d525eb83b0f46cb5df73a39b1f5ec30ae
OLEG <oskaryahoo@gmail.com>
2026-01-29 23:13:01 +0300

    create pom.xml

---
+++ src/test/java/com/oskarvos/apptestspring/AppTestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oskarvos.apptestspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTestSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
